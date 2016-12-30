ff1d709e0ab26fdc1f4702a7e97c7cd0a1220428
Gokhan Tamkoc <gokhantamkoc@gmail.com>
2016-12-31 00:42:27 +0300

    added character encoding configuration

--- src/main/java/com/tamkoc/product/compendium/initialization/AppInitializer.java
+++ src/main/java/com/tamkoc/product/compendium/initialization/AppInitializer.java
@@ -1,5 +1,7 @@
 package com.tamkoc.product.compendium.initialization;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
@@ -8,7 +10,10 @@ import com.tamkoc.product.compendium.configuration.WebAppConfiguration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.EnumSet;
 
 /**
  * @author Gokhan Tamkoc on 08.10.2016.
@@ -20,9 +25,20 @@ public class AppInitializer implements WebApplicationInitializer {
 
     public void onStartup(ServletContext servletContext)
             throws ServletException {
+
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.register(WebAppConfiguration.class);
         servletContext.addListener(new ContextLoaderListener(ctx));
+
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = servletContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+
 
         ctx.setServletContext(servletContext);
 
